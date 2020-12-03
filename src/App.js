5514aeb4567fada377e14557fdcdc7edd07d5fce
Ali Mahmoud <961alimahmoud@gmail.com>
2020-12-03 14:57:05 -0500

    created main page container

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Contacts from './containers/contact.js'
 import Portfolio from './containers/portfolio.js'
+import Index from './containers/index.js'
 
 function App() {
   return (
     <div>
-      <Portfolio />
+      <Index />
     </div>
   );
 }
