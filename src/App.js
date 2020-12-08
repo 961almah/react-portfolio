14ff87e37b899db2037d5ea39ee256264414c207
Ali Mahmoud <961alimahmoud@gmail.com>
2020-12-08 17:56:30 -0500

    creating homepage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import Contacts from './containers/contact.js'
 import Portfolio from './containers/portfolio.js'
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import Index from './containers/index.js'
 
 function App() {
   return (
     <div>
-      <Index />
+      <BrowserRouter><Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Link to="/home">Home</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/portfolio">Portfolio</Link>
+        </Nav>
+      </Navbar>
+        <Route path="/home"><Index /></Route>
+        <Route path="/contact"><Contacts /></Route>
+        <Route path="/portfolio"><Portfolio /></Route>
+      </BrowserRouter>
     </div>
   );
 }
